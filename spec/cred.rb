ec9347b5144678801bdcd33b068cf84f3df561ed
DashaGerasimova <dashagera@gmail.com>
2017-12-07 18:07:03 +0300

    Added expires_in and created_at to token's payload

---
+++ spec/cred.rb
@@ -0,0 +1,7 @@
+client_id = '2bfc86fcc644e64bea842a971fa6d19ea1c837de018cadffa711866fed6d47e1'
+client_secret = '87a8d5fed8f3ca363efe63b430c0bcfbaf154306e945b55b0be6af905b2a2299'
+redirect_uri = 'http://localhost:3000/'
+site = 'http://localhost:3001/'
+client = OAuth2::Client.new(client_id, client_secret, :site => site)
+client.auth_code.authorize_url(:redirect_uri => redirect_uri)
+token = client.auth_code.get_token(code, :redirect_uri => redirect_uri)
